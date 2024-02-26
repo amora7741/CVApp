fb889c8129fa439999bf0a05cbbd6c7a4181c69b
amora7741 <mora7741@gmail.com>
2024-02-26 14:31:12 -0800

    Move state up to app instead of info component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,65 @@
 import { useState } from 'react';
-
 import './App.css';
 import GeneralInfo from './components/GeneralInfo';
+
+import DisplayInfo from './components/DisplayInfo';
 import InfoContainer from './components/InfoContainer';
 
 function App() {
+  const [isEditMode, setIsEditMode] = useState(true);
+  const [cvInfo, setCvInfo] = useState({
+    generalInfo: {},
+    schoolInfo: {},
+    practicalExperience: {},
+  });
+
+  const toggleEditMode = () => {
+    setIsEditMode(!isEditMode);
+  };
+
+  const handleInfoChange = (section, info) => {
+    setCvInfo({
+      ...cvInfo,
+      [section]: info,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsEditMode(false);
+  };
+
   return (
     <>
       <InfoContainer>
-        <GeneralInfo></GeneralInfo>
+        {isEditMode && (
+          <div className='forms'>
+            <h1>CV Application</h1>
+            <form onSubmit={handleSubmit}>
+              <GeneralInfo
+                generalInfo={cvInfo.generalInfo}
+                onChange={(info) => handleInfoChange('generalInfo', info)}
+              />
+              {/* <SchoolInfo
+                schoolInfo={cvInfo.schoolInfo}
+                onChange={(info) => handleInfoChange('schoolInfo', info)}
+              />
+              <PracticalExperience
+                practicalExperience={cvInfo.practicalExperience}
+                onChange={(info) =>
+                  handleInfoChange('practicalExperience', info)
+                }
+              /> */}
+              <button type='submit'>Submit</button>
+            </form>
+          </div>
+        )}
+        {!isEditMode && (
+          <div>
+            <DisplayInfo {...cvInfo} />
+            <button onClick={toggleEditMode}>Edit</button>
+          </div>
+        )}
       </InfoContainer>
     </>
   );
--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,67 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function GeneralInfo() {
-  const [generalInfo, setGeneralInfo] = useState({
-    name: '',
-    email: '',
-    phoneNumber: '',
-  });
-
-  const [isEditing, setIsEditing] = useState(true);
-
-  const handleChange = (e) => {
-    setGeneralInfo({
+function GeneralInfo({ generalInfo, onChange }) {
+  const handleChange = (event) => {
+    onChange({
       ...generalInfo,
-      [e.target.name]: e.target.value,
+      [event.target.name]: event.target.value,
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setIsEditing(false);
-  };
-
-  const handleEdit = () => {
-    setIsEditing(true);
-  };
-
   return (
-    <>
-      <h2>General Info</h2>
-      {isEditing ? (
-        <form onSubmit={handleSubmit}>
-          <input
-            type='text'
-            name='name'
-            value={generalInfo.name}
-            onChange={handleChange}
-            placeholder='Name'
-          />
-          <input
-            type='email'
-            name='email'
-            value={generalInfo.email}
-            onChange={handleChange}
-            placeholder='Email'
-          />
-          <input
-            type='tel'
-            name='phoneNumber'
-            value={generalInfo.phoneNumber}
-            onChange={handleChange}
-            placeholder='Phone Number'
-          />
-          <button type='submit'>Submit</button>
-        </form>
-      ) : (
-        <div>
-          <p>Name: {generalInfo.name}</p>
-          <p>Email: {generalInfo.email}</p>
-          <p>Phone Number: {generalInfo.phoneNumber}</p>
-          <button onClick={handleEdit}>Edit</button>
-        </div>
-      )}
-    </>
+    <section className='generalInfo'>
+      <input
+        type='text'
+        name='name'
+        value={generalInfo.name || ''}
+        onChange={handleChange}
+        placeholder='Name'
+      />
+      <input
+        type='email'
+        name='email'
+        value={generalInfo.email || ''}
+        onChange={handleChange}
+        placeholder='Email'
+      />
+      <input
+        type='tel'
+        name='phone'
+        value={generalInfo.phone || ''}
+        onChange={handleChange}
+        placeholder='Phone'
+      />
+    </section>
   );
 }
 
