2090ee8cdd596924a4b0764bcc5a834a8951c36a
amora7741 <mora7741@gmail.com>
2024-02-27 11:54:54 -0800

    Add moving labels to practical info

--- src/components/PracticalInfo.jsx
+++ src/components/PracticalInfo.jsx
@@ -8,39 +8,65 @@ function PracticalInfo({ practicalInfo, onChange }) {
 
   return (
     <section className='practicalInfo'>
-      <input
-        type='text'
-        name='companyName'
-        value={practicalInfo.companyName || ''}
-        onChange={handleChange}
-        placeholder='Company Name'
-      />
-      <input
-        type='text'
-        name='positionTitle'
-        value={practicalInfo.positionTitle || ''}
-        onChange={handleChange}
-        placeholder='Position Title'
-      />
-      <input
-        type='text'
-        name='responsibilities'
-        value={practicalInfo.responsibilities || ''}
-        onChange={handleChange}
-        placeholder='Responsibilities'
-      />
-      <input
-        type='date'
-        name='startDate'
-        value={practicalInfo.startDate || ''}
-        onChange={handleChange}
-      />
-      <input
-        type='date'
-        name='endDate'
-        value={practicalInfo.endDate || ''}
-        onChange={handleChange}
-      />
+      <div className='fieldholder'>
+        <input
+          id='companyName'
+          type='text'
+          name='companyName'
+          value={practicalInfo.companyName || ''}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor='companyName'>Company Name</label>
+      </div>
+      <div className='fieldholder'>
+        <input
+          id='positionTitle'
+          type='text'
+          name='positionTitle'
+          value={practicalInfo.positionTitle || ''}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor='positionTitle'>Position Title</label>
+      </div>
+      <div className='fieldholder'>
+        <input
+          id='responsibilities'
+          type='text'
+          name='responsibilities'
+          value={practicalInfo.responsibilities || ''}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor='responsibilities'>Responsibilities</label>
+      </div>
+      <div className='fieldholder'>
+        <input
+          id='startDate'
+          type='date'
+          name='startDate'
+          value={practicalInfo.startDate || ''}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor='startDate' className='datelabel'>
+          Start Date
+        </label>
+      </div>
+      <div className='fieldholder'>
+        <input
+          id='endDate'
+          type='date'
+          name='endDate'
+          value={practicalInfo.endDate || ''}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor='endDate' className='datelabel'>
+          End Date
+        </label>
+      </div>
     </section>
   );
 }
