d5d4a801a48bb21f223c6a12a272eb4220f49d78
amora7741 <mora7741@gmail.com>
2024-02-27 12:20:22 -0800

    Add header styling for cvpage

--- src/components/DisplayInfo.jsx
+++ src/components/DisplayInfo.jsx
@@ -2,30 +2,36 @@ import React from 'react';
 
 function DisplayInfo({ generalInfo, educationalInfo, practicalInfo }) {
   return (
-    <div className='cvPage'>
-      <h1>Submitted Information</h1>
-      <div>
-        <h3>General Information</h3>
-        <p>Name: {generalInfo.name}</p>
-        <p>Email: {generalInfo.email}</p>
-        <p>Phone: {generalInfo.phone}</p>
-      </div>
-      <div>
-        <h3>Educational Information</h3>
-        <p>School Name: {educationalInfo.schoolName}</p>
-        <p>Study Title: {educationalInfo.studyTitle}</p>
-        <p>Start Date: {educationalInfo.studyStart}</p>
-        <p>End Date: {educationalInfo.studyEnd}</p>
-      </div>
-      <div>
-        <h3>Practical Experience</h3>
-        <p>Company Name: {practicalInfo.companyName}</p>
-        <p>Position Title: {practicalInfo.positionTitle}</p>
-        <p>Responsibilities: {practicalInfo.responsibilities}</p>
+    <div className='cvpage'>
+      <header>
+        <hr />
+        <h1 className='pink'>{generalInfo.name}</h1>
+        <hr />
+        <hr className='thickhr' />
         <p>
-          Worked from {practicalInfo.startDate} to {practicalInfo.endDate}
+          {generalInfo.email} <span className='pink'>◆</span>{' '}
+          {generalInfo.phone}{' '}
         </p>
-      </div>
+      </header>
+
+      <main>
+        <div>
+          <h3>Educational Information</h3>
+          <p>School Name: {educationalInfo.schoolName}</p>
+          <p>Study Title: {educationalInfo.studyTitle}</p>
+          <p>Start Date: {educationalInfo.studyStart}</p>
+          <p>End Date: {educationalInfo.studyEnd}</p>
+        </div>
+        <div>
+          <h3>Practical Experience</h3>
+          <p>Company Name: {practicalInfo.companyName}</p>
+          <p>Position Title: {practicalInfo.positionTitle}</p>
+          <p>Responsibilities: {practicalInfo.responsibilities}</p>
+          <p>
+            Worked from {practicalInfo.startDate} to {practicalInfo.endDate}
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
