4b4a4eea9b87ac83812fff008daba19cc22beac0
amora7741 <mora7741@gmail.com>
2024-02-26 18:13:45 -0800

    Display all the entered info

--- src/components/DisplayInfo.jsx
+++ src/components/DisplayInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function DisplayInfo({ generalInfo, educationInfo, practicalInfo }) {
+function DisplayInfo({ generalInfo, educationalInfo, practicalInfo }) {
   return (
     <div>
       <h1>Submitted Information</h1>
@@ -9,6 +9,22 @@ function DisplayInfo({ generalInfo, educationInfo, practicalInfo }) {
         <p>Name: {generalInfo.name}</p>
         <p>Email: {generalInfo.email}</p>
         <p>Phone: {generalInfo.phone}</p>
+      </div>
+      <div>
+        <h3>Educational Information</h3>
+        <p>School Name: {educationalInfo.schoolName}</p>
+        <p>Study Title: {educationalInfo.studyTitle}</p>
+        <p>Start Date: {educationalInfo.studyStart}</p>
+        <p>End Date: {educationalInfo.studyEnd}</p>
+      </div>
+      <div>
+        <h3>Practical Experience</h3>
+        <p>Company Name: {practicalInfo.companyName}</p>
+        <p>Position Title: {practicalInfo.positionTitle}</p>
+        <p>Responsibilities: {practicalInfo.responsibilities}</p>
+        <p>
+          Worked from {practicalInfo.startDate} to {practicalInfo.endDate}
+        </p>
       </div>
     </div>
   );
