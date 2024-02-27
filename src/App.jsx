2a0da8c1f006e18577cf2ad4ecde842f97e24522
amora7741 <mora7741@gmail.com>
2024-02-27 10:51:34 -0800

    Remove unneeded div

--- src/App.jsx
+++ src/App.jsx
@@ -46,61 +46,57 @@ function App() {
     <>
       <InfoContainer>
         {isEditMode ? (
-          <div className='forms'>
-            <form onSubmit={handleSubmit}>
-              {currentSection === 0 && (
-                <>
-                  <h1>General Info</h1>
-                  <GeneralInfo
-                    generalInfo={cvInfo.generalInfo}
-                    onChange={(info) => handleInfoChange('generalInfo', info)}
-                  />
-                </>
-              )}
-              {currentSection === 1 && (
-                <>
-                  <h1>Educational Info</h1>
-                  <EducationalInfo
-                    educationalInfo={cvInfo.educationalInfo}
-                    onChange={(info) =>
-                      handleInfoChange('educationalInfo', info)
-                    }
-                  />
-                </>
-              )}
-              {currentSection === 2 && (
-                <>
-                  <h1>Practical Experience</h1>
-                  <PracticalInfo
-                    practicalInfo={cvInfo.practicalInfo}
-                    onChange={(info) => handleInfoChange('practicalInfo', info)}
-                  />
-                </>
-              )}
+          <form onSubmit={handleSubmit}>
+            {currentSection === 0 && (
+              <>
+                <h1>General Info</h1>
+                <GeneralInfo
+                  generalInfo={cvInfo.generalInfo}
+                  onChange={(info) => handleInfoChange('generalInfo', info)}
+                />
+              </>
+            )}
+            {currentSection === 1 && (
+              <>
+                <h1>Educational Info</h1>
+                <EducationalInfo
+                  educationalInfo={cvInfo.educationalInfo}
+                  onChange={(info) => handleInfoChange('educationalInfo', info)}
+                />
+              </>
+            )}
+            {currentSection === 2 && (
+              <>
+                <h1>Practical Experience</h1>
+                <PracticalInfo
+                  practicalInfo={cvInfo.practicalInfo}
+                  onChange={(info) => handleInfoChange('practicalInfo', info)}
+                />
+              </>
+            )}
 
-              <div className='buttons'>
-                {currentSection > 0 && (
-                  <button
-                    type='button'
-                    onClick={handlePreviousSection}
-                    className='paging'
-                  >
-                    <img src={LeftArrow} alt='' />
-                  </button>
-                )}
-                {currentSection < 2 && (
-                  <button
-                    type='button'
-                    onClick={handleNextSection}
-                    className='paging'
-                  >
-                    <img src={RightArrow} alt='' />
-                  </button>
-                )}
-                {currentSection === 2 && <button type='submit'>Submit</button>}
-              </div>
-            </form>
-          </div>
+            <div className='buttons'>
+              {currentSection > 0 && (
+                <button
+                  type='button'
+                  onClick={handlePreviousSection}
+                  className='paging'
+                >
+                  <img src={LeftArrow} alt='' />
+                </button>
+              )}
+              {currentSection < 2 && (
+                <button
+                  type='button'
+                  onClick={handleNextSection}
+                  className='paging'
+                >
+                  <img src={RightArrow} alt='' />
+                </button>
+              )}
+              {currentSection === 2 && <button type='submit'>Submit</button>}
+            </div>
+          </form>
         ) : (
           <div className='cvContainer'>
             <DisplayInfo {...cvInfo} />
